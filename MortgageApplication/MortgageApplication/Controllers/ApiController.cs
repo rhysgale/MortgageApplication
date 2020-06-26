eb50333a6380aee5cbb445fd290260d5749c8562
Rhys <rhys.gale@symphonyretailai.com>
2020-06-27 00:24:23 +0100

    Call external API. Website to handle the data displaying and sending.

--- MortgageApplication/MortgageApplication/Controllers/ApiController.cs
+++ MortgageApplication/MortgageApplication/Controllers/ApiController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models.Request;
+using Models.Response;
+using Newtonsoft.Json;
 
 namespace MortgageApplication.Controllers
 {
@@ -12,5 +14,32 @@ namespace MortgageApplication.Controllers
     public class ApiController : ControllerBase
     {
 
+        [HttpPost("NewUser")]
+        public async Task<NewUserResponse> NewUser(NewUserRequest request)
+        {
+            using var httpClient = new HttpClient();
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+            using var response = await httpClient.PostAsync("https://localhost:44396/user/newuser", content);
+            
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<NewUserResponse>(apiResponse);
+        }
+
+        [HttpPost("GetEligibleProducts")]
+        public async Task<EligibleProductsResponse> GetEligibleProducts(EligibleProductsRequest request)
+        {
+            using var httpClient = new HttpClient();
+            
+            StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+            using var response = await httpClient.PostAsync("https://localhost:44396/Products/EligibleProducts", content);
+            
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<EligibleProductsResponse>(apiResponse);
+        }
     }
 }
--- MortgageApplication/MortgageApplication/wwwroot/Custom/Index.js
+++ MortgageApplication/MortgageApplication/wwwroot/Custom/Index.js
@@ -2,17 +2,51 @@
     el: '#main',
     data: {
         showUserInput: false,
+        showProductSearch: false,
         firstName: "",
         lastName: "",
-        dateOfBirth: "",
-        emailAddress: ""
+        dateOfBirth: "1900-01-01",
+        emailAddress: "",
+        currentUserId: "",
+        price: 0,
+        deposit: 0,
+        banksWithProducts: ""
     },
     methods: {
         applicationStart: function () {
             this.showUserInput = true;
         },
-        submitInformation: function () {
+        submitInformation: async function () {
+            var model = { FirstName: firstName.value, LastName: lastName.value, DateOfBirth: dateOfBirth.value, Email: emailAddress.value };
 
+            var options = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(model)
+            };
+
+            var response = await fetch("https://localhost:44371/api/NewUser", options);
+
+            var data = await response.json();
+
+            currentUserId = data.newUserId;
+            this.showProductSearch = true;
+            this.showUserInput = false;
+        },
+        fetchProducts: async function () {
+            var model = { UserId: currentUserId, Deposit: parseFloat(deposit.value), HouseValue: parseFloat(price.value) };
+
+            var options = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(model)
+            };
+
+            var response = await fetch("https://localhost:44371/api/GetEligibleProducts", options);
+
+            var data = await response.json();
+
+            this.banksWithProducts = data.banksWithProducts;
         }
     }
 });
